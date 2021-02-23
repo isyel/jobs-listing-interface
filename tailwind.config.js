622a955f8ab5040d715d3262b3222359eb1ab17a
isyel <isykaal4saviour@gmail.com>
2021-02-23 08:13:04 +0100

    #️⃣ configured tailwindcss for production - set configuration for purging css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,8 @@
 module.exports = {
-	purge: ["/src/**/*.jsx", "./index.js"],
+	purge: {
+		enabled: true,
+		content: ["./pages/*.js", "./components/*.js"],
+	},
 	darkMode: false, // or 'media' or 'class'
 	theme: {
 		extend: {},
