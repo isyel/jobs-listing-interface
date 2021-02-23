88fb4883fc2f45d373500b54bf690e20f41fe6ed
isyel <isykaal4saviour@gmail.com>
2021-02-23 10:19:17 +0100

    🌎 Corrected production URL

--- config/index.js
+++ config/index.js
@@ -2,4 +2,4 @@ const dev = process.env.NODE_ENV !== "production";
 
 export const server = dev
 	? "http://localhost:3000"
-	: "https://jobs-listing-interface-4zvsiz1ro-isyel.vercel.app";
+	: "https://jobs-listing-interface.vercel.app/";
